3e230265f3ceed1bf8a6648e25906379b196e33a
Wilmer Bandres <wilmer0593@hotmail.com>
2013-12-01 11:48:41 -0430

    comienzo

---
+++ CF/aux/D.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<cstdio>
+#include<cstring>
+#include<vector>
+#include<string>
+#include<utility>
+using namespace std;
+
+int main(){
+
+	return 0;
+}
